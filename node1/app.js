b23937a00e5c18ea4c69c025b11ebb3acea22235
zxx <>
2017-10-20 20:43:44 +0800

    new file

---
+++ node1/app.js
@@ -0,0 +1,7 @@
+//对象
+var stuff=require("./module");
+// console.log(counter(["Henry","2","3"]));
+// console.log(counter(3,5));
+console.log(stuff.adder(3,5));
+console.log(stuff.counter(["1","2"]));
+console.log(stuff.pi);
