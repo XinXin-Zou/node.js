39dee59d345955dc20fd08038fa82f36fbaa4cc4
zxx <>
2017-10-20 20:47:51 +0800

    new file

---
+++ node3/readStream.js
@@ -0,0 +1,10 @@
+var fs=require("fs");
+var myReadStream=fs.createReadStream(__dirname+"/readMe.txt","utf8");
+// console.log(myReadStream);
+
+//必须写data
+//分拨传
+myReadStream.on("data",function(chunk){
+	console.log('=============接收chunk============');
+	console.log(chunk);
+});
